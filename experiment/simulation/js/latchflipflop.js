e2ce01fd18784e8228f0fa86d50818276a3c0d5d
Venneti Sri Satya Vinay <81536213+srisatyavinay@users.noreply.github.com>
2022-04-13 22:06:07 +0530

    Add all files of exp - 6

--- experiment/simulation/js/latchflipflop.js
+++ experiment/simulation/js/latchflipflop.js
@@ -1,38 +1,42 @@
-function latchvalidate() {
-  x = permutator([0, 1])
+function latchValidate() {
+  const x = permutator([0, 1])
   // console.log(x)
-  circuit_valid = 0
+  circuitValid = 0
   for (let i = 0; i < x.length; i++) {
-    if (jsmap.has("input0$mux0") && jsmap.has("clock0$mux0") && jsmap.has("inverter" + x[i][1] + "$mux0") && jsmap.has("mux0$inverter" + x[i][0]) && jsmap.has("inverter0$inverter" + x[i][1]) && jsmap.has("inverter" + x[i][0] + "$output0") && (jsmap.size == 6)) {
-      circuit_valid = 1
+    if (connectionMap.has("input0$mux0") && connectionMap.has("clock0$mux0") && connectionMap.has("inverter" + x[i][1] + "$mux0") && connectionMap.has("mux0$inverter" + x[i][0]) && connectionMap.has("inverter0$inverter" + x[i][1]) && connectionMap.has("inverter" + x[i][0] + "$output0") && (connectionMap.size === 6)) {
+      circuitValid = 1
       break
     }
   }
-  if (circuit_valid) {
+  if (circuitValid) {
     document.getElementById("graph-image").src = "./images/Screenshot (168).png"
     document.getElementById("graph-image").style.display = "block";
+    changeObservation("&#10004; Circuit is correct", 'text-danger', 'text-success')
   }
   else {
     // document.getElementById("graph-image").alt = "The graph is not shown since the circuit connection is incorrect"
     document.getElementById("graph-image").style.display = "none";
-    alert("The circuit is wrong");
+    changeObservation("&#10060; Circuit is incorrect", 'text-success', 'text-danger')
   }
 }
-function selectedvald() {
-  if (selected_tab == 0) {
-    latchvalidate()
+function circuitValidate() {
+  if (selectedTab === currentTab.LATCH) {
+    latchValidate()
   }
   else {
-    flipflopvalidate()
+    flipflopValidate()
   }
+  document.getElementById('error-container').style = 'display:none;';
 }
 function permutator(inputArr) {
-  var results = [];
+  const results = [];
 
   function permute(arr, memo) {
-    var cur, memo = memo || [];
+    let cur
 
-    for (var i = 0; i < arr.length; i++) {
+    memo = memo || [];
+
+    for (let i = 0; i < arr.length; i++) {
       cur = arr.splice(i, 1);
       if (arr.length === 0) {
         results.push(memo.concat(cur));
@@ -46,23 +50,29 @@ function permutator(inputArr) {
 
   return permute(inputArr);
 }
-function flipflopvalidate() {
-  x = permutator([0, 1])
+function flipflopValidate() {
+  const x = permutator([0, 1])
   // console.log(x)
-  circuit_valid = 0
+  circuitValid = 0
   for (let i = 0; i < x.length; i++) {
-    if (jsmap.has("input0$latch" + x[i][0]) && jsmap.has("clock0$latch" + x[i][0]) && jsmap.has("latch" + x[i][0] + "$latch" + x[i][1]) && jsmap.has("latch" + x[i][1] + "$output0") && jsmap.has("clockbar0$latch" + x[i][1]) && (jsmap.size == 5)) {
-      circuit_valid = 1
+    if (connectionMap.has("input0$latch" + x[i][0]) && connectionMap.has("clock0$latch" + x[i][0]) && connectionMap.has("latch" + x[i][0] + "$latch" + x[i][1]) && connectionMap.has("latch" + x[i][1] + "$output0") && connectionMap.has("clockbar0$latch" + x[i][1]) && (connectionMap.size === 5)) {
+      circuitValid = 1
       break
     }
   }
-  if (circuit_valid) {
+  if (circuitValid) {
     document.getElementById("graph-image").src = "./images/Screenshot (169).png"
     document.getElementById("graph-image").style.display = "block";
+    changeObservation("&#10004; Circuit is correct", 'text-danger', 'text-success')
   }
   else {
-    // document.getElementById("graph-image").alt = "The graph is not shown since the circuit connection is incorrect"
     document.getElementById("graph-image").style.display = "none";
-    // alert("The circuit is wrong");
+    changeObservation("&#10060; Circuit is incorrect", 'text-success', 'text-danger')
   }
+}
+
+function changeObservation(htmlText, removedClass, addedClass) {
+  document.getElementById("output-text").innerHTML = htmlText
+  document.getElementById("output-text").classList.remove(removedClass)
+  document.getElementById("output-text").classList.add(addedClass)
 }
