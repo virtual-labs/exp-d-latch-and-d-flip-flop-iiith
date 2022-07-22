3b111075670ae6244966e52e66c34acc594e965d
Aditya Malhotra <aadityamalhotra1512@gmail.com>
2022-07-22 08:36:47 +0530

    Testing (#6)
    * Add all files
    
    * Add images
    
    * Make a change
    
    * Add all files of exp - 6
    
    * Make the latest changes
    
    * demo completed
    
    * improved animation and removed previous code
    
    * fixed practice section
    
    * fixed indentation
    
    * fixed experiment descriptor
    
    * fixed experiment descriptor
    
    * code refactoring
    
    * code refactor
    
    * code refactoring
    
    * fixed reset
    
    Co-authored-by: Venneti Sri Satya Vinay <81536213+srisatyavinay@users.noreply.github.com>
    Co-authored-by: gautamxyz <gautamghai02@gmail.com>

---
+++ experiment/simulation/js/integrate.js
@@ -0,0 +1,294 @@
+'use strict';
+import { componentsList, selectedTab, currentTab } from './main.js';
+import { addInstanceClock, addInstanceClockbar, addInstanceFinalInput, addInstanceInverter, addInstanceLatch, addInstanceMux, addInstanceFinalOutput } from './components.js';
+import { jsplumbInstance } from "./components.js";
+import { latchValidate, flipflopValidate } from "./latchflipflop.js";
+
+window.compInverter = compInverter;
+window.compMux = compMux;
+window.compLatch = compLatch;
+window.compClock = compClock;
+window.compClockbar = compClockbar;
+window.circuitValidate = circuitValidate;
+
+let count = { PMOS: 0, NMOS: 0, VDD: 0, Ground: 0, Inverter: 0, Mux: 0, Latch: 0, Transistor: 0, Clock: 0, Clockbar: 0 };
+let maxCount = { PMOS: 0, NMOS: 0, VDD: 0, Ground: 0, Inverter: 2, Mux: 2, Latch: 2, Transistor: 0, Clock: 1, Clockbar: 1 };
+
+export function resetCounts() {
+    count = { PMOS: 0, NMOS: 0, VDD: 0, Ground: 0, Inverter: 0, Mux: 0, Latch: 0, Transistor: 0, Clock: 0, Clockbar: 0 };
+    if (selectedTab === currentTab.LATCH) {
+        maxCount = { PMOS: 0, NMOS: 0, VDD: 0, Ground: 0, Inverter: 2, Mux: 1, Latch: 0, Transistor: 0, Clock: 1, Clockbar: 0 };
+    }
+    else {
+        maxCount = { PMOS: 0, NMOS: 0, VDD: 0, Ground: 0, Inverter: 0, Mux: 0, Latch: 2, Transistor: 0, Clock: 1, Clockbar: 1 };
+    }
+
+}
+
+export function circuitValidate() {
+    if (selectedTab === currentTab.LATCH) {
+        latchValidate();
+    }
+    else {
+        flipflopValidate();
+    }
+    document.getElementById('error-container').style = 'display:none;';
+}
+
+export function compInverter() {
+    maxCount.Inverter -= 1;
+    if (maxCount.Inverter < 0) {
+        document.getElementById('error-container').style.display = 'flex';
+        return;
+    }
+    const id = "inverter" + count.Inverter;
+    const divPushed = document.createElement('div');
+    divPushed.innerHTML = `
+        <svg xmlns="https://www.w3.org/2000/svg" xmlns:xlink="https://www.w3.org/1999/xlink" version="1.1" viewBox="-0.5 -0.5 84 53">
+            <g class="demo-transistor">
+                <path d="M 1 26 L 21 26"/>
+                <path d="M 21 1 L 61 26 L 21 51 Z" />
+                <ellipse cx="66" cy="26" rx="5" ry="5"/>
+                <path d="M 71 26 L 81 26"/>
+            </g>
+        </svg>`;
+    divPushed.id = id;
+    divPushed.className = 'component';
+    count.Inverter += 1;
+    const container = document.getElementById("diagram");
+    container.insertAdjacentElement("afterbegin", divPushed);
+    jsplumbInstance.draggable(id, { "containment": true });
+    addInstanceInverter(id);
+    componentsList.push(divPushed);
+}
+
+export function compMux() {
+    maxCount.Mux -= 1;
+    if (maxCount.Mux < 0) {
+        document.getElementById('error-container').style.display = 'flex';
+        return;
+    }
+    const id = "mux" + count.Mux;
+    const divPushed = document.createElement('div');
+    divPushed.innerHTML = `
+        <svg xmlns="https://www.w3.org/2000/svg" xmlns:xlink="https://www.w3.org/1999/xlink" version="1.1" viewBox="-0.5 10 124 143">
+            <g class="demo-transistor">
+                <rect x="21" y="21" width="80" height="120"/>
+                <g transform="translate(-0.5 -0.5)">
+                    <switch>
+                        <foreignObject pointer-events="none" width="100%" height="100%" requiredFeatures="https://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow: visible; text-align: left;">
+                            <div xmlns="https://www.w3.org/1999/xhtml" style="display: flex; align-items: unsafe center; justify-content: unsafe center; width: 78px; height: 1px; padding-top: 81px; margin-left: 22px;">
+                                <div data-drawio-colors="color: rgb(0, 0, 0); " style="box-sizing: border-box; font-size: 0px; text-align: center;">
+                                    <div style="display: inline-block; font-size: 12px; font-family: Helvetica; color: rgb(0, 0, 0); line-height: 1.2; pointer-events: all; white-space: normal; overflow-wrap: normal;">\
+                                        <b style="font-size: 14px">2 * 1<br />I/O<br />MUX</b>
+                                    </div>
+                                </div>
+                            </div>
+                        </foreignObject>
+                        <text x="61" y="85" fill="rgb(0, 0, 0)" font-family="Helvetica" font-size="12px" text-anchor="middle">2 * 1...</text>
+                    </switch>
+                </g>
+                <path d="M 1 101 L 21 101"/>
+                <path d="M 121 81 L 101 81"/>
+                <path d="M 61 1 L 61 21"/>
+                <path d="M 1 51 L 21 51"/>
+            </g>
+            <switch>
+                <g requiredFeatures="https://www.w3.org/TR/SVG11/feature#Extensibility"/>
+                <a transform="translate(0,-5)" xlink:href="https://www.diagrams.net/doc/faq/svg-export-text-problems" target="_blank">
+                    <text text-anchor="middle" font-size="10px" x="50%" y="100%">Text is not SVG - cannot display</text>
+                </a>
+            </switch>
+        </svg>`;
+    divPushed.id = id;
+    divPushed.className = 'component2';
+    count.Mux += 1;
+    const container = document.getElementById("diagram");
+    container.insertAdjacentElement("afterbegin", divPushed);
+    jsplumbInstance.draggable(id, { "containment": true });
+    addInstanceMux(id);
+    componentsList.push(divPushed);
+}
+
+export function compLatch() {
+    maxCount.Latch -= 1;
+    if (maxCount.Latch < 0) {
+        document.getElementById('error-container').style.display = 'flex';
+        return;
+    }
+    const id = "latch" + count.Latch;
+    const divPushed = document.createElement('div');
+    divPushed.innerHTML = `
+        <svg xmlns="https://www.w3.org/2000/svg" xmlns:xlink="https://www.w3.org/1999/xlink" version="1.1" viewBox="-0.5 10 124 143">
+            <g class="demo-transistor">
+                <rect x="21" y="21" width="80" height="120" fill="none" stroke="rgb(0, 0, 0)" stroke-width="3" pointer-events="all"/>
+                <g transform="translate(-0.5 -0.5)">
+                    <switch>
+                        <foreignObject pointer-events="none" width="100%" height="100%" requiredFeatures="https://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow: visible; text-align: left;">
+                            <div xmlns="https://www.w3.org/1999/xhtml" style="display: flex; align-items: unsafe center; justify-content: unsafe center; width: 78px; height: 1px; padding-top: 81px; margin-left: 22px;">
+                                <div data-drawio-colors="color: rgb(0, 0, 0); " style="box-sizing: border-box; font-size: 0px; text-align: center;">
+                                    <div style="display: inline-block; font-size: 12px; font-family: Helvetica; color: rgb(0, 0, 0); line-height: 1.2; pointer-events: all; white-space: normal; overflow-wrap: normal;">
+                                        <b style="font-size: 14px">1 * 1<br />I/O<br />Latch</b>
+                                    </div>
+                                </div>
+                            </div>
+                        </foreignObject>
+                        <text x="61" y="85" fill="rgb(0, 0, 0)" font-family="Helvetica" font-size="12px" text-anchor="middle">1 * 1...</text>
+                    </switch>
+                </g>
+                <path d="M 21 81 L 1 81" fill="none" stroke="rgb(0, 0, 0)" stroke-width="3" stroke-miterlimit="10" pointer-events="stroke"/>
+                <path d="M 121 81 L 101 81" fill="none" stroke="rgb(0, 0, 0)" stroke-width="3" stroke-miterlimit="10" pointer-events="stroke"/>
+                <path d="M 61 1 L 61 21" fill="none" stroke="rgb(0, 0, 0)" stroke-width="3" stroke-miterlimit="10" pointer-events="stroke"/>
+            </g>
+            <switch>
+                <g requiredFeatures="https://www.w3.org/TR/SVG11/feature#Extensibility"/>
+                <a transform="translate(0,-5)" xlink:href="https://www.diagrams.net/doc/faq/svg-export-text-problems" target="_blank">
+                    <text text-anchor="middle" font-size="10px" x="50%" y="100%">Text is not SVG - cannot display</text>
+                </a>
+            </switch>
+        </svg>`;
+    divPushed.id = id;
+    divPushed.className = 'component2';
+    count.Latch += 1;
+    const container = document.getElementById("diagram");
+    container.insertAdjacentElement("afterbegin", divPushed);
+    jsplumbInstance.draggable(id, { "containment": true });
+    addInstanceLatch(id);
+    componentsList.push(divPushed);
+}
+
+export function compClock() {
+    maxCount.Clock -= 1;
+    if (maxCount.Clock < 0) {
+        document.getElementById('error-container').style.display = 'flex';
+        return;
+    }
+    const id = "clock" + count.Clock;
+    const divPushed = document.createElement('div');
+    divPushed.innerHTML = `
+        <svg xmlns="https://www.w3.org/2000/svg" xmlns:xlink="https://www.w3.org/1999/xlink"
+            version="1.1" viewBox="-0.5 -0.5 64 34">
+            <g class="demo-transistor">
+                <path d="M 1 31 L 41 31" />
+                <path d="M 1 1 L 61 1" />
+                <path d="M 1 31 L 1 1" />
+                <path d="M 41 31 L 61 1" />
+                <g transform="translate(-0.5 -0.5)">
+                    <switch>
+                        <foreignObject pointer-events="none" width="100%" height="100%"
+                            requiredFeatures="https://www.w3.org/TR/SVG11/feature#Extensibility"
+                            style="overflow: visible; text-align: left;">
+                            <div xmlns="https://www.w3.org/1999/xhtml"
+                                style="display: flex; align-items: unsafe center; justify-content: unsafe center; width: 1px; height: 1px; padding-top: 21px; margin-left: 23px;">
+                                <div data-drawio-colors="color: rgb(0, 0, 0); "
+                                    style="box-sizing: border-box; font-size: 0px; text-align: center;">
+                                    <div
+                                        style="display: inline-block; font-size: 14px; font-family: Helvetica; color: rgb(0, 0, 0); line-height: 1.2; pointer-events: all; white-space: nowrap;">
+                                        <b>Clk</b>
+                                    </div>
+                                </div>
+                            </div>
+                        </foreignObject><text x="23" y="25" fill="rgb(0, 0, 0)"
+                            font-family="Helvetica" font-size="14px" text-anchor="middle">Clk</text>
+                    </switch>
+                </g>
+            </g>
+            <switch>
+                <g requiredFeatures="https://www.w3.org/TR/SVG11/feature#Extensibility" /><a
+                    transform="translate(0,-5)"
+                    xlink:href="https://www.diagrams.net/doc/faq/svg-export-text-problems"
+                    target="_blank"><text text-anchor="middle" font-size="10px" x="50%"
+                        y="100%">Text is not SVG - cannot display</text></a>
+            </switch>
+        </svg>`;
+    divPushed.id = id;
+    divPushed.className = 'component';
+    count.Clock += 1;
+    const container = document.getElementById("diagram");
+    container.insertAdjacentElement("afterbegin", divPushed);
+    jsplumbInstance.draggable(id, { "containment": true });
+    addInstanceClock(id);
+    componentsList.push(divPushed);
+}
+export function compClockbar() {
+    maxCount.Clockbar -= 1;
+    if (maxCount.Clockbar < 0) {
+        document.getElementById('error-container').style.display = 'flex';
+        return;
+    }
+    const id = "clockbar" + count.Clockbar;
+    const divPushed = document.createElement('div');
+    divPushed.innerHTML = `
+        <svg xmlns="https://www.w3.org/2000/svg" xmlns:xlink="https://www.w3.org/1999/xlink" version="1.1" viewBox="-0.5 -0.5 64 34">
+            <g class="demo-transistor">
+                <path d="M 1 31 L 41 31" />
+                <path d="M 1 1 L 61 1" />
+                <path d="M 1 31 L 1 1" />
+                <path d="M 41 31 L 61 1" />
+                <g transform="translate(-0.5 -0.5)">
+                    <switch>
+                        <foreignObject pointer-events="none" width="100%" height="100%"
+                            requiredFeatures="https://www.w3.org/TR/SVG11/feature#Extensibility"
+                            style="overflow: visible; text-align: left;">
+                            <div xmlns="https://www.w3.org/1999/xhtml"
+                                style="display: flex; align-items: unsafe center; justify-content: unsafe center; width: 1px; height: 1px; padding-top: 21px; margin-left: 23px;">
+                                <div data-drawio-colors="color: rgb(0, 0, 0); "
+                                    style="box-sizing: border-box; font-size: 0px; text-align: center;">
+                                    <div
+                                        style="display: inline-block; font-size: 14px; font-family: Helvetica; color: rgb(0, 0, 0); line-height: 1.2; pointer-events: all; white-space: nowrap;">
+                                        <div style="text-decoration:overline"><b>Clk</b></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </foreignObject><text x="23" y="25" fill="rgb(0, 0, 0)"
+                            font-family="Helvetica" font-size="14px" text-anchor="middle">Clk</text>
+                    </switch>
+                </g>
+            </g>
+            <switch>
+                <g requiredFeatures="https://www.w3.org/TR/SVG11/feature#Extensibility" /><a
+                    transform="translate(0,-5)"
+                    xlink:href="https://www.diagrams.net/doc/faq/svg-export-text-problems"
+                    target="_blank"><text text-anchor="middle" font-size="10px" x="50%"
+                        y="100%">Text is not SVG - cannot display</text></a>
+            </switch>
+        </svg>`;
+    divPushed.id = id;
+    divPushed.className = 'component';
+    count.Clockbar += 1;
+    const container = document.getElementById("diagram");
+    container.insertAdjacentElement("afterbegin", divPushed);
+    jsplumbInstance.draggable(id, { "containment": true });
+    addInstanceClockbar(id);
+    componentsList.push(divPushed);
+}
+
+export function comp2Input0() {
+    const id = "input0";
+    const divPushed = document.createElement('div');
+    divPushed.innerHTML = 'Input 1<br><br>';
+    divPushed.id = id;
+    divPushed.className = 'io-component';
+    divPushed.style.top = "1.25rem";
+    divPushed.style.left = "0.625rem";
+    const container = document.getElementById("diagram");
+    container.insertAdjacentElement("afterbegin", divPushed);
+    jsplumbInstance.draggable(id, { "containment": true });
+    addInstanceFinalInput(id);
+    componentsList.push(divPushed);
+}
+
+export function comp2Output() {
+    const id = "output0";
+    const divPushed = document.createElement('div');
+    divPushed.innerHTML = 'Output<br><br>';
+    divPushed.id = id;
+    divPushed.className = 'io-component';
+    divPushed.style.top = "1.25rem";
+    divPushed.style.right = "0.625rem";
+    const container = document.getElementById("diagram");
+    container.insertAdjacentElement("afterbegin", divPushed);
+    jsplumbInstance.draggable(id, { "containment": true });
+    addInstanceFinalOutput(id);
+    componentsList.push(divPushed);
+}
---
+++ experiment/simulation/js/latchflipflop.js
@@ -0,0 +1,82 @@
+'use strict';
+import { connectionMap } from "./main.js";
+function checkConnectionsLatch(x, i) {
+  return (connectionMap.has("input0$mux0") && connectionMap.has("clock0$mux0")
+    && connectionMap.has("inverter" + x[i][1] + "$mux0") && connectionMap.has("mux0$inverter" + x[i][0])
+    && connectionMap.has("inverter0$inverter" + x[i][1]) && connectionMap.has("inverter" + x[i][0] + "$output0")
+    && (connectionMap.size === 6))
+}
+export function latchValidate() {
+  const x = permutator([0, 1]);
+  // console.log(x)
+  let circuitValid = 0;
+  for (let i = 0; i < x.length; i++) {
+    if (checkConnectionsLatch(x, i)) {
+      circuitValid = 1;
+      break;
+    }
+  }
+  if (circuitValid) {
+    document.getElementById("graph-image").src = "./images/screenshot_168.png";
+    document.getElementById("graph-image").style.display = "block";
+    changeObservation("&#10004; Circuit is correct", 'text-danger', 'text-success');
+  }
+  else {
+    document.getElementById("graph-image").style.display = "none";
+    changeObservation("&#10060; Circuit is incorrect", 'text-success', 'text-danger');
+  }
+}
+
+export function permutator(inputArr) {
+  const results = [];
+
+  function permute(arr, memo) {
+    let cur;
+
+    memo = memo || [];
+
+    for (let i = 0; i < arr.length; i++) {
+      cur = arr.splice(i, 1);
+      if (arr.length === 0) {
+        results.push(memo.concat(cur));
+      }
+      permute(arr.slice(), memo.concat(cur));
+      arr.splice(i, 0, cur[0]);
+    }
+
+    return results;
+  }
+
+  return permute(inputArr);
+}
+function checkConnectionsFlipFlop(x, i) {
+  return (connectionMap.has("input0$latch" + x[i][0]) && connectionMap.has("clock0$latch" + x[i][0])
+    && connectionMap.has("latch" + x[i][0] + "$latch" + x[i][1]) && connectionMap.has("latch" + x[i][1] + "$output0")
+    && connectionMap.has("clockbar0$latch" + x[i][1]) && (connectionMap.size === 5))
+}
+export function flipflopValidate() {
+  const x = permutator([0, 1]);
+  // console.log(x)
+  let circuitValid = 0;
+  for (let i = 0; i < x.length; i++) {
+    if (checkConnectionsFlipFlop(x, i)) {
+      circuitValid = 1;
+      break;
+    }
+  }
+  if (circuitValid) {
+    document.getElementById("graph-image").src = "./images/screenshot_169.png";
+    document.getElementById("graph-image").style.display = "block";
+    changeObservation("&#10004; Circuit is correct", 'text-danger', 'text-success');
+  }
+  else {
+    document.getElementById("graph-image").style.display = "none";
+    changeObservation("&#10060; Circuit is incorrect", 'text-success', 'text-danger');
+  }
+}
+
+export function changeObservation(htmlText, removedClass, addedClass) {
+  document.getElementById("output-text").innerHTML = htmlText;
+  document.getElementById("output-text").classList.remove(removedClass);
+  document.getElementById("output-text").classList.add(addedClass);
+}
