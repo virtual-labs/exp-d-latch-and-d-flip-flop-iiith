475c752efdbaf06b56c04dc3874494b3239b708e
Ananya Vaibhavi Pabba <anivaibh24@gmail.com>
2023-06-22 18:10:31 +0530

    Added Negative and positive edge flip flop experiments in Practice and procedure sections

--- experiment/simulation/js/latchflipflop.js
+++ experiment/simulation/js/latchflipflop.js
@@ -1,5 +1,6 @@
 'use strict';
 import { connectionMap } from "./main.js";
+import { selectedTab, currentTab } from './main.js';
 function checkConnectionsLatch(x, i) {
   return (connectionMap.has("input0$mux0") && connectionMap.has("clock0$mux0")
     && connectionMap.has("inverter" + x[i][1] + "$mux0") && connectionMap.has("mux0$inverter" + x[i][0])
@@ -48,30 +49,52 @@ export function permutator(inputArr) {
 
     return permute(inputArr);
 }
-function checkConnectionsFlipFlop(x, i) {
-  return (connectionMap.has("input0$latch" + x[i][0]) && connectionMap.has("clock0$latch" + x[i][0])
-    && connectionMap.has("latch" + x[i][0] + "$latch" + x[i][1]) && connectionMap.has("latch" + x[i][1] + "$output0")
-    && connectionMap.has("clockbar0$latch" + x[i][1]) && (connectionMap.size === 5))
-}
 export function flipflopValidate() {
-  const x = permutator([0, 1]);
-  // console.log(x)
+  const permutatorMap = permutator([0, 1]);
+  document.getElementById('error-container').style = 'display:none;';
   let circuitValid = 0;
-  for (let i = 0; i < x.length; i++) {
-    if (checkConnectionsFlipFlop(x, i)) {
-      circuitValid = 1;
-      break;
+  for (let i = 0; i < permutatorMap.length; i++) {
+    if (connectionMap.has("input0$latch" + permutatorMap[i][0]) && connectionMap.has("clock0$latch" + permutatorMap[i][0]) && connectionMap.has("latch" + permutatorMap[i][0] + "$latch" + permutatorMap[i][1]) && connectionMap.has("latch" + permutatorMap[i][1] + "$output0") && connectionMap.has("clockbar0$latch" + permutatorMap[i][1]) && (connectionMap.size === 5) && selectedTab === currentTab.NEG_FLIPFLOP) {
+        circuitValid = 1;
+        break;
     }
+    if (connectionMap.has("input0$latch" + permutatorMap[i][0]) && connectionMap.has("clockbar0$latch" + permutatorMap[i][0]) && connectionMap.has("latch" + permutatorMap[i][0] + "$latch" + permutatorMap[i][1]) && connectionMap.has("latch" + permutatorMap[i][1] + "$output0") && connectionMap.has("clock0$latch" + permutatorMap[i][1]) && (connectionMap.size === 5) && selectedTab === currentTab.POS_FLIPFLOP) {
+        circuitValid = 1;
+        break;
+    }
+}
+if (circuitValid) {
+
+  const imagePaths = {
+      1: {
+          1: "./images/inputWave1Neg.png",
+          2: "./images/inputWave2Neg.png",
+          3: "./images/inputWave3Neg.png",
+          4: "./images/inputWave4Neg.png"
+      },
+      2: {
+          1: "./images/inputWave1Pos.png",
+          2: "./images/inputWave2Pos.png",
+          3: "./images/inputWave3Pos.png",
+          4: "./images/inputWave4Pos.png"
+      }
   }
-  if (circuitValid) {
-    document.getElementById("graph-image").src = "./images/screenshot_169.png";
-    document.getElementById("graph-image").style.display = "block";
-    changeObservation("&#10004; Circuit is correct", 'text-danger', 'text-success');
-  }
-  else {
-    document.getElementById("graph-image").style.display = "none";
-    changeObservation("&#10060; Circuit is incorrect", 'text-success', 'text-danger');
-  }
+
+  let m = document.getElementById("input-selector").value;
+  const divGraphImage = document.getElementById("graph-image")
+  divGraphImage.src = imagePaths[selectedTab][m];
+  divGraphImage.style.display = 'block';
+  document.getElementById("output-box").style.display = "block";
+
+  changeObservation("&#10004; Circuit is correct", 'text-danger', 'text-success')
+}
+else {
+  const divGraphImage = document.getElementById("graph-image")
+  divGraphImage.src = "";
+  divGraphImage.style.display = 'none';
+  document.getElementById("output-box").style.display = "none";
+  changeObservation("&#10060; Circuit is incorrect", 'text-success', 'text-danger');
+}
 }
 
 export function changeObservation(htmlText, removedClass, addedClass) {
